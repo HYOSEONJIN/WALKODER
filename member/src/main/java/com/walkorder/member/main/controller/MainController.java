6841cad467a7c49920907874c75dc9390e8691a7
HYOSEONJIN <71997946+HYOSEONJIN@users.noreply.github.com>
2021-03-18 17:39:46 +0900

    springboot / header&footer로 분리
    스프링부트

--- member/src/main/java/com/walkorder/member/main/controller/MainController.java
+++ member/src/main/java/com/walkorder/member/main/controller/MainController.java
@@ -2,6 +2,8 @@ package com.walkorder.member.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -9,6 +11,11 @@ public class MainController {
     @RequestMapping(value = "/")
     public String main() {
         return "index";
+    }
+
+    @RequestMapping(value = "/memberMain")
+    public String memberMain() {
+        return "member";
     }
 
 }
