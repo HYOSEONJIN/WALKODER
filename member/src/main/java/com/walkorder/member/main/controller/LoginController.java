d999630a1f641ec5c3155f0ad7ef192715523af6
HYOSEONJIN <71997946+HYOSEONJIN@users.noreply.github.com>
2021-03-27 18:55:58 +0900

    카카오 로그인 진행중

--- member/src/main/java/com/walkorder/member/main/controller/LoginController.java
+++ member/src/main/java/com/walkorder/member/main/controller/LoginController.java
@@ -5,14 +5,26 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.walkorder.member.main.service.KakaoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@Slf4j
 public class LoginController {
+
+    final String RESTAPI_KEY = "0e5bc43cde12fc5035c512eca57aa8be";
+    final String REDIRECT_URI = "http://localhost:8080/kakao/callback";
+
+    @Autowired
+    private KakaoService kakaoservice;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login() {
@@ -21,9 +33,6 @@ public class LoginController {
 
     @RequestMapping(value = "/kakao", method = RequestMethod.GET)
     public String kakaoConnect() {
-
-        final String RESTAPI_KEY = "0e5bc43cde12fc5035c512eca57aa8be";
-        final String REDIRECT_URI = "http://localhost:8080/kakao/callback";
 
         StringBuffer url = new StringBuffer();
 
@@ -39,10 +48,10 @@ public class LoginController {
             RequestMethod.POST })
     public String kakaoLogin(@RequestParam("code") String code, RedirectAttributes ra, HttpSession session,
             HttpServletResponse response) throws IOException {
+        // http://localhost:8080/kakao/callback?code=RTPCN_ZskA6OY87e5i1D80olEgRWEWxEu-vPkLEbvJL6OEBcMi5w7VC5Lqvp6SxB_yfLXAopb1UAAAF4clS7QA
 
-        // http://localhost:8080/login/kakao/callback?code=RTPCN_ZskA6OY87e5i1D80olEgRWEWxEu-vPkLEbvJL6OEBcMi5w7VC5Lqvp6SxB_yfLXAopb1UAAAF4clS7QA
-
-        System.out.println(code);
+        log.info("kakao code : ", code);
+        String accessToken = kakaoservice.getAccessTOken(code, RESTAPI_KEY);
 
         return "index";
     }
